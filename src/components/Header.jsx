7646c45d1b93b1a9e6224fbb5c90a361bc894e0c
apricot <just.anastasia.terenteva@gmail.com>
2023-03-04 11:05:36 +1100

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Header() {
+    return (
+        <>
+            <div className="log-in">log into your monash account</div>
+
+            <div className="title">Monash Eats!</div>
+        </>
+
+    );
+}
+
+
+export default Header;
